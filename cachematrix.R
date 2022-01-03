5d3a5f63158640ca44c5edea562a6ca211761525
JaakkoMatiasIkonen <96998691+JaakkoMatiasIkonen@users.noreply.github.com>
2022-01-03 20:04:53 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The makeCacheMatrix function creates the matrix that is later cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL # first we initialize "inverse" and "x"
+  set <- function(y){
+    x <- y
+    inverse <<- NULL
+  }
+  get <- function(){x} 
+  set_inv <- functon(inv) {inverse <<- inv}
+  get_inv <- function(){inverse}
+  list(set=set, 
+       get=get, 
+       set_inv=set_inv, 
+       get_inv=get_inv) ### a new object is returned to the parent environemnt 
 }
 
+## The cacheSolve function retrives the cached matrix 
+## (as calculated by makeCacheMatrix earlier)
+## and calculates its matrix
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ##receiving cache data
+  inverse <- x$get_inv()
+  if(!is.null(inverse)){
+    message("receiving cache data") ## checking whether the inverse is null
+    return(inverse) 
+  }
+  matrix <- x$get()
+  inverse <- solve(matrix, ...)
+  x$set_inv(inverse)
+  inverse ##returns the inverse of the matrix
 }
+
+##EXAMPLE##
+gmatrix <- makeCacheMatrix(matrix(1:4, nrow=2, ncol=2)) ##gen 2x2 matrix
+gmatrix$get() 
+gmatrix$get_inv()
+cacheSolve(gmatrix) # inverse!
+cacheSolve(gmatrix) # as the matrix remains the same, cacheSolve retrieves the inverse from the cache!
